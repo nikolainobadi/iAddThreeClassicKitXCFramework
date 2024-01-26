ee1b09a8b24d9bc75882297d75c32faa3e48fc8e
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-01-26 12:17:18 -0800

    Update Package.swift
    update reference to binary target

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "iAddThreeClassicKit",
-            url: "https://github.com/nikolainobadi/iAddThreeClassicKit/releases/download/v1.0.0/iAddThreeClassicKit.xcframework.zip",
+            url: "https://github.com/nikolainobadi/iAddThreeClassicKitXCFramework/releases/download/v0.8.0/iAddThreeClassicKit.xcframework.zip",
             checksum: "5bbc4a68bc529cc8615a03b804f27fee3c371d45d0691c9c0557e146c6e6bd26"
         )
     ]
